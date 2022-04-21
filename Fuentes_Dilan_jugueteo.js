25b25d2859018fab2625006b0e48091586393193
DilanInsaggi <dilan.fuentes.silva@alumnos.sip.cl>
2022-04-21 09:23:09 -0400

    Fuentes_Dilan_JS_Jugueteo

---
+++ Fuentes_Dilan_jugueteo.js
@@ -0,0 +1,7 @@
+function muestraSiElNiñoPuedeSubirAlaMontañaRusa (EntradaAlturaEdad) {
+    //EntradaAlturaEdad = 60; 
+    if (EntradaAlturaEdad > 52 )
+    console.log ('¡ subete, chico!');
+    else(EntradaAlturaEdad < 52)
+    console.log(' Lo siento, chico. Tal Vez el proximo año'); 
+}
